d33c95683892758648156e73e19997f88ef9fb46
Juan Sebastian Gomez Torres <jgomez@easysol.net>
2017-05-13 19:04:07 -0500

    bookmark fragment creation

--- WeatherApp/app/src/main/java/com/backbase/weatherchallenge/HomeActivity.java
+++ WeatherApp/app/src/main/java/com/backbase/weatherchallenge/HomeActivity.java
@@ -1,12 +1,11 @@
 package com.backbase.weatherchallenge;
-
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.backbase.weatherchallenge.dummy.DummyContent;
 import com.google.android.gms.maps.model.LatLng;
 
-public class HomeActivity extends AppCompatActivity implements MapWeatherFragment.OnFragmentInteractionListener {
+public class HomeActivity extends AppCompatActivity implements MapWeatherFragment.OnFragmentInteractionListener, BookmarkWeatherFragment.OnListFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +15,11 @@ public class HomeActivity extends AppCompatActivity implements MapWeatherFragmen
 
     @Override
     public void onMapFragmentMapTouch(LatLng point) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
 
     }
 }
