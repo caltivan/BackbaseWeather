829545bfef1a8860e63557dbfd89e99619f59aa2
Juan Sebastian Gomez Torres <jgomez@easysol.net>
2017-05-15 08:38:24 -0500

    add help toolbar icoon

--- WeatherApp/app/src/main/java/com/backbase/weather_challenge/view/HomeActivity.java
+++ WeatherApp/app/src/main/java/com/backbase/weather_challenge/view/HomeActivity.java
@@ -5,6 +5,8 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.backbase.weather_challenge.R;
 import com.backbase.weather_challenge.model.Bookmark;
@@ -45,6 +47,27 @@ public class HomeActivity extends AppCompatActivity implements BookmarkMapFragme
         bookmarkMapFragment.loadBookmarkMapMarks();
         bookmarkMapFragment.focusMapPosition(bookmark.latitude, bookmark.longitude);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_help) {
+
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void showCityForecastDialog(int index) {
